54133548067236e0e3654891a3ca132b6a9f0a8c
pugnator14 <vladmurgoci@gmail.com>
2021-01-10 19:52:32 +0200

    Server 0.2

--- app.js
+++ app.js
@@ -5,8 +5,17 @@ var port = process.argv[2];
 var app = express();
 
 app.use(express.static(__dirname + "/public"));
+
+app.use(express.static('public', {
+    extensions: ['html'],
+}));
+
 http.createServer(app).listen(port);
 
+app.get('/play', function (req, res) {
+    res.redirect('game');
+});
+
 app.get('/', function(req, res) {
-    res.redirect("splash.html");
-})
+    res.redirect("splash");
+});
--- public/javascripts/splash.js
+++ public/javascripts/splash.js
@@ -1,6 +1,6 @@
 //After document is loaded, get playBtn and add event listener which redirects on click to game.html
 document.addEventListener('DOMContentLoaded', function () {
     document.getElementById('playBtn').addEventListener('click', function () {
-        location.href = 'game.html';
+        location.href = 'game';
     })
 });
