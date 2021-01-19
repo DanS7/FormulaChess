fc22272d79e610a186b8c63d44197b5c79d75f4f
Dan Savastre <dantheodor123@gmail.com>
2021-01-19 02:58:56 +0200

    Splash Screen updated

--- public/javascripts/splash.js
+++ public/javascripts/splash.js
@@ -1,8 +1,22 @@
-//After document is loaded, get playBtn and add event listener which redirects on click to game.html
-/*document.addEventListener('DOMContentLoaded', function () {
-    document.getElementById('playBtn').addEventListener('click', function () {
-        location.href = 'game';
-    })
-});*/
+document.addEventListener('DOMContentLoaded', spawnRandomPieces);
 
+
+const whitePawn = 'images/White Pieces/Pawn_White.png';
+const container = document.getElementsByClassName('pieces')[0];
+
+function getRandom(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+}
+
+function spawnRandomPieces() {
+    for(let i = 0; i < 10; i++) {
+        for(let j = 0; j < 10; j++) {
+            // let pieceNumber = getRandom(12);                 // 12 = number of pieces: 6 white 6 black
+            let piece = document.createElement('img');
+            piece.src = whitePawn;
+            piece.className = 'bgPiece';
+            container.appendChild(piece);
+        }
+    }
+}
 
